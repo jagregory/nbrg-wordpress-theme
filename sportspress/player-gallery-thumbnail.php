070797a1d27c75bb1b9ca2f144c4f1876a1c7cee
James Gregory <james@jagregory.com>
2014-08-25 21:57:12 +1000

    Initial commit

---
+++ sportspress/player-gallery-thumbnail.php
@@ -0,0 +1,16 @@
+<div class="player-thumbnail large-4 medium-6 small-12 columns left">
+  <a href="<?php echo get_post_permalink( $id ); ?>">
+    <?php
+    $number = get_post_meta( $id, 'sp_number', true );
+    if ( $number != null ): ?>
+      <span class="player-number"><?php echo $number; ?></span>
+    <?php endif; ?>
+    <?php
+    if( has_post_thumbnail( $id ) )
+      echo get_the_post_thumbnail( $id, 'sportspress-square-thumbnail', array( 'class' => 'profile-photo fill-image' ) );
+    else
+      echo '<img width="150" height="150" src="http://www.gravatar.com/avatar/?s=150&d=mm&f=y" class="attachment-thumbnail wp-post-image profile-photo fill-image">';
+    ?>
+    <h5 class="image-caption-alt"><?php echo get_the_title( $id ); ?></h5>
+  </a>
+</div>
---
+++ sportspress/single-player.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * The Template for displaying all single players
+ *
+ * @package WordPress
+ * @subpackage NBRG
+ * @since NBRG 1.0.1
+ * @version 1.0.1
+ */
+
+get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+<?php
+  $player = new SP_Player( $post );
+
+  $show_nationality_flags = get_option( 'sportspress_player_show_flags', 'yes' ) == 'yes' ? true : false;
+
+  $number = get_post_meta( $post->ID, 'sp_number', true );
+  $nationality = get_post_meta( $post->ID, 'sp_nationality', true );
+  $metrics_before = $player->metrics( true );
+  $metrics_after = $player->metrics( false );
+  $positions = get_the_terms( $post->ID, 'sp_position' );
+  $leagues = get_the_terms( $post->ID, 'sp_league' );
+  $current_team = get_post_meta( $post->ID, 'sp_current_team', true );
+  
+  $country_name = tb_array_value( SP()->countries->countries, $nationality, null );
+
+  $common = array();
+
+  // $common[ __( 'Nationality', 'sportspress' ) ] = $country_name ? ( $show_nationality_flags ? '<img src="' . plugin_dir_url( SP_PLUGIN_FILE ) . '/assets/images/flags/' . strtolower( $nationality ) . '.png" alt="' . $nationality . '"> ' : '' ) . $country_name : '&mdash;';
+
+  $data = array_merge( $metrics_before, $common, $metrics_after );
+
+  if ( $positions )
+    $infobox_classes = 'large-6 large-pull-3';
+  else
+    $infobox_classes = 'large-9';
+?>
+
+<div class="infinity team-name">
+  <div class="row">
+    <div class="small-9 columns">
+      <h2><a href="<?php echo get_permalink( $current_team ); ?>"><?php the_title(); ?></a></h2>
+    </div>
+    <div class="small-3 columns">
+      <a href="<?php echo get_post_permalink( $current_team ); ?>" class="team-logo"><?php echo get_the_post_thumbnail( $current_team, 'sportspress-fit-thumbnail' ); ?></a>
+    </div>
+  </div>
+</div><!-- .infinity -->
+
+<div id="content" class="site-content" role="main">
+  <div class="player-main">
+    <div class="row">
+      <div class="large-3 medium-6 columns">
+        <div class="player-photo">
+          <?php the_post_thumbnail( 'sportspress-square-thumbnail', array( 'class' => 'fill-image' ) ); ?>
+          <?php if ( $number != null ): ?>
+          <h5 class="image-caption">
+            <?php _e( 'Number', 'nbrg' ); ?>
+            <span class="player-number"><?php echo $number; ?></span>
+          </h5>
+          <?php endif; ?>
+        </div>
+      </div><!-- .columns -->
+      <div class="large-3 large-push-6 medium-6 columns">
+        <?php
+        if ( $positions ):
+          foreach( $positions as $position ):
+            $args = array(
+              'post_type' => 'attachment',
+              'post_status' => 'any',
+              'posts_per_page' => -1,
+              'tax_query' => array(
+                array(
+                  'taxonomy' => 'sp_position',
+                  'field' => 'id',
+                  'terms' => $position->term_id,
+                ),
+              ),
+            );
+            $attachments = get_posts( $args );
+
+            ?>
+            <div class="player-position">
+              <?php
+              foreach( $attachments as $attachment ):
+                echo wp_get_attachment_image( $attachment->ID, 'sportspress-square-thumbnail', array( 'class' => 'fill-image' ) );
+              endforeach;
+              ?>
+              <h5 class="image-caption"><?php echo $position->name; ?></h5>
+            </div>
+            <?php
+          endforeach;
+        endif;
+        ?>
+      </div><!-- .columns -->
+      <?php if ( get_the_content() ): ?>
+      <div class="<?php echo $infobox_classes; ?> columns">
+        <div class="infobox">
+          <div class="entry-content">
+            <h2><?php _e( 'Bio', 'nbrg' ); ?></h2>
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div><!-- .columns -->
+      <?php endif; ?>
+    </div><!-- .row -->
+  </div><!-- .player-main -->
+  <div class="row">
+    <div class="large-6 columns">
+      <div class="infobox">
+        <h2><?php _e( 'Stats', 'nbrg' ); ?></h2>
+        <div class="row">
+          <?php foreach ( $data as $label => $value ): if ( $value == null ) continue; ?>
+            <div class="large-6 medium-3 columns">
+              <small><?php echo $label; ?></small>
+              <h4><?php echo $value; ?></h4>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div><!-- .columns -->
+    <div class="<?php echo get_the_content() ? 'large-6' : 'large-12'; ?> columns">
+      <?php sp_get_template( 'player-statistics.php' ); ?>
+      <?php if ( has_excerpt() ): ?>
+      <div class="sp-excerpt">
+        <?php echo apply_filters( 'the_content', get_the_excerpt() ); ?>
+      </div>
+      <?php endif; ?>
+    </div><!-- .columns -->
+  </div><!-- .row -->
+</div><!-- #content -->
+<?php endwhile; ?>
+
+<?php get_footer();

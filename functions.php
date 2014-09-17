3008ed987496e70aa73022684f447b266ee9ef3c
James Gregory <james@jagregory.com>
2014-09-18 09:07:42 +1000

    Custom thumbnail size for player gallery, so it doesnt cut everyones heads off

---
+++ functions.php
@@ -0,0 +1,2 @@
+<?php
+add_image_size('nbrg-player-thumbnail', 320, 320, true, array('left', 'top'));
--- sportspress/player-gallery-thumbnail.php
+++ sportspress/player-gallery-thumbnail.php
@@ -5,7 +5,7 @@
  * @package WordPress
  * @subpackage NBRG
  * @since NBRG 1.0.1
- * @version 1.0.1
+ * @version 1.0.2
  */
 ?>
 <div class="player-thumbnail large-4 medium-6 small-12 columns left">
@@ -17,7 +17,7 @@
     <?php endif; ?>
     <?php
     if( has_post_thumbnail( $id ) )
-      echo get_the_post_thumbnail( $id, 'sportspress-square-thumbnail', array( 'class' => 'profile-photo fill-image' ) );
+      echo get_the_post_thumbnail( $id, 'nbrg-player-thumbnail', array( 'class' => 'profile-photo fill-image' ) );
     else
       echo '<img width="150" height="150" src="http://www.gravatar.com/avatar/?s=150&d=mm&f=y" class="attachment-thumbnail wp-post-image profile-photo fill-image">';
     ?>

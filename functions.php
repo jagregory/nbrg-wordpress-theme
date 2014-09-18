eaee2a8f32ff3235e3b2b5358cfeb054a202c713
James Gregory <james@jagregory.com>
2014-09-18 22:07:33 +1000

    Custom homepage template with a single post and unique sidebar

--- functions.php
+++ functions.php
@@ -1,2 +1,12 @@
 <?php
 add_image_size('nbrg-player-thumbnail', 320, 320, array('center', 'top'));
+
+register_sidebar( array(
+  'name'        => __( 'Single Post Home Sidebar', 'nbrg' ),
+  'id'        => 'single-post-home',
+  'description'     => __( 'Single Post Homepage sidebar.', 'nbrg' ),
+  'before_widget'   => '<aside id="%1$s" class="widget %2$s">',
+  'after_widget'    => '</aside>',
+  'before_title'    => '<h2 class="widget-title">',
+  'after_title'     => '</h2>',
+) );
---
+++ template-single-post-home.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Single Post Home
+ *
+ * @package WordPress
+ * @subpackage Premier
+ * @since Premier 1.0
+ */
+
+get_header(); ?>
+
+<div id="content" class="site-content" role="main">
+  <div class="row">
+    <div class="large-7 columns">
+      <?php
+      // show the home page content
+      while ( have_posts() ) {
+        the_post();
+        get_template_part( 'content', 'page' );
+      }
+   
+      // show the latest post
+      query_posts( 'posts_per_page=1' );
+      while ( have_posts() ) {
+        the_post();
+        get_template_part( 'content', get_post_type() );
+      }
+      ?>
+      <a href="/posts" class="more-link"><span class="icon-book"></span>Read more recent news</a>
+    </div>
+    <div class="large-5 columns">
+      <?php if ( is_active_sidebar( 'single-post-home' ) ) : ?>
+      <div id="homepage-widgets" class="homepage-widgets widget-area">
+        <?php dynamic_sidebar( 'single-post-home' ); ?>
+      </div><!-- #homepage-widgets -->
+      <?php endif; ?>
+      <?php do_action('themeboy_after_content'); ?>
+    </div>
+  </div><!-- .row -->
+</div><!-- #content -->
+
+<?php get_footer();
